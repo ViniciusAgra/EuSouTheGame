56c4e1668d02370624699a0162f1c4ae7d6b22c9
ViniciusAgra <viniciusagra2015@gmail.com>
2024-10-30 17:11:12 -0300

    Atualização 10.2 - Musica Tema Implementada e Suporte Para Musica do Jogo Posteriormente

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import flet as ft
+import pygame
+import time
 
 from telas.cadastro import CadastroScreen
 from telas.configura import ConfiguraScreen
@@ -10,8 +12,34 @@ from telas.temas import TemasScreen
 from telas.jogo import JogoScreen
 from data.database import Database
 
+# Inicializa o Pygame
+pygame.mixer.init()
+
+def start_music():
+    pygame.mixer.music.load("audio/MTQuemSouEu.mp3")  
+    pygame.mixer.music.set_volume(0.1)  
+    pygame.mixer.music.play(-1)  
+
+def game_music():
+    
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load("audio\MTQuemSouEu.mp3")
+    pygame.mixer.music.set_volume(1)
+    pygame.mixer.music.play(1)
+    
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)  # Espera 100ms antes de checar novamente
+
+    # Para a segunda música
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load("audio\MTQuemSouEu.mp3")
+    pygame.mixer.music.set_volume(1)
+    pygame.mixer.music.play(-1)
+
 def main(page: ft.Page):
     db = Database()
+    start_music()  # Inicia a música padrão ao abrir o app
+
     def navigate(screen, **kwargs):
         page.clean()
         if screen == "splash":
@@ -24,6 +52,7 @@ def main(page: ft.Page):
             CadastroScreen(page, navigate).show()
         elif screen == "jogo":
             JogoScreen(page, navigate)
+            game_music()  # Música para o jogo
         elif screen == "login":
             LoginScreen(page, navigate).show()
         elif screen == "personalizado":
